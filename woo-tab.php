8320e8b011f567eecd9450f0cf14c9a46b4e74f4
Gabriel <gabriellmcoelho@gmail.com>
2024-01-31 22:54:18 -0300

    Version 1.2.1 - Add License

--- woo-tab.php
+++ woo-tab.php
@@ -1,9 +1,10 @@
 <?php
 /*
 Plugin Name: Quick Access For Woocommerce
-Plugin URI: https://gabriellmcoelho.space/
+Plugin URI: https://github.com/gabriellmcoelho/woo-tab
 Description: This plugin adds woocommerce and POS shortcuts
-Version: 1.2
+Version: 1.2.1
+License: GPLv2 or later
 Author: Gabriel Coelho
 Author URI: https://gabriellmcoelho.space/
 */
